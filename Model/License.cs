c860c44d280bb9751c4d7816520604182c9999aa
Antonin Rouxel <antonin.rouxel44@outlook.fr>
2022-10-16 00:30:43 +0200

    Github API

---
+++ Model/License.cs
@@ -0,0 +1,11 @@
+namespace anrouxel.github.io.Model
+{
+    public class License
+    {
+        public string? key {get; set;}
+        public string? name {get; set;}
+        public string? spdx_id {get; set;}
+        public string? url {get; set;}
+        public string? node_id {get; set;}
+    }
+}
