06e8b17157bd919e73360a3e34d941484804d225
Antonin Rouxel <antonin.rouxel44@outlook.fr>
2023-11-04 21:30:47 +0100

    Initial Commit v2

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using anrouxel.github.io;
-using Radzen;
 using System.Globalization;
 using Microsoft.JSInterop;
+using MudBlazor.Services;
+using anrouxel.github.io;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,10 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddScoped<DialogService>();
-builder.Services.AddScoped<NotificationService>();
-builder.Services.AddScoped<TooltipService>();
-builder.Services.AddScoped<ContextMenuService>();
+builder.Services.AddMudServices();
 
 builder.Services.AddLocalization();
 
