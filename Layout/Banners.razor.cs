1afeab0ac0db03f8265ecd594a9f0a9ca068ea45
Antonin Rouxel <antonin.rouxel44@outlook.fr>
2024-03-05 19:20:23 +0100

    Add banner

---
+++ Layout/Banners.razor.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Components;
+
+namespace anrouxel.Layout
+{
+    public partial class Banners
+    {
+        [Parameter]
+        public string? Title { get; set; }
+    }
+}
