06dd3d98e1df74fadd40704b2196b3f426656c52
Antonin Rouxel <antonin.rouxel44@outlook.fr>
2024-03-07 17:48:05 +0100

    Mise à jour des paramètres GitHub et des services GitHub

--- Services/GithubService.cs
+++ Services/GithubService.cs
@@ -7,35 +7,40 @@ namespace anrouxel.Services
 {
     public class GithubService : IGithubService
     {
+        private readonly GithubSettings _githubSettings;
+
         private readonly HttpClient _httpClient;
 
         public GithubService(
             IOptions<GithubSettings> githubSettings
         ) {
+            _githubSettings = githubSettings.Value;
             _httpClient = new HttpClient();
             _httpClient.BaseAddress = new Uri(githubSettings.Value.GITHUB_API_URL);
+        }
 
-            Console.WriteLine($"Github API URL: {githubSettings.Value.GITHUB_API_URL}");
-            Console.WriteLine($"HttpClient BaseAddress: {_httpClient.BaseAddress}");
+        private string MakeUrl(string? path)
+        {
+            if (path == null)
+            {
+                return _githubSettings.GITHUB_PROFILE_URL;
+            }
+            return $"{_githubSettings.GITHUB_PROFILE_URL}/{path}";
         }
 
         public async Task<Profile?> GetProfileAsync()
         {
-            return await _httpClient.GetFromJsonAsync<Profile>("");
+            return await _httpClient.GetFromJsonAsync<Profile>(MakeUrl(null));
         }
 
-        public async Task<Repository[]?> GetRepositoriesAsync()
+        public async Task<List<Repository>> GetRepositoriesAsync()
         {
-            return await _httpClient.GetFromJsonAsync<Repository[]>("/repos");
-        }
-
-        public async Task<string?> GetAvatarAsync()
-        {
-            var avatar_url = await _httpClient.GetFromJsonAsync<Profile>("");
-
-            Console.WriteLine(avatar_url!.avatar_url);
-
-            return avatar_url!.avatar_url;
+            var repositories = await _httpClient.GetFromJsonAsync<List<Repository>>(MakeUrl("repos"));
+            if (repositories == null)
+            {
+                return new List<Repository>();
+            }
+            return repositories;
         }
     }
 }
